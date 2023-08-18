0336c823daad7e64610b7ab5a542badf5beaae60
Rabten Samphel <rabten@protonmail.com>
2023-08-18 18:05:16 -0400

    added card drop logic css

--- src/components/cvbuilder.js
+++ src/components/cvbuilder.js
@@ -1,20 +1,76 @@
+import { useState } from "react";
+
 function CVbuidler() {
+  const [cardOpen, setCardOpen] = useState(false);
+
+  // this function opens the card and displays the input fields
+  function openCard() {
+    setCardOpen(!cardOpen);
+
+    if (cardOpen) {
+      document.getElementById('education').style.height = '5rem'
+    } else if (!cardOpen) {
+      document.getElementById('education').style.height = '40rem'
+    }
+  }
+
   return (
     <div className="cvbuilder">
-      <form className="form" id='personal'>
+      <form className="form" id="personal">
         <h1>Personal Details</h1>
 
-        <label for="fullName">Full Name:</label>
+        <label htmlFor="fullName">Full Name:</label>
         <input id="fullName"></input>
 
-        <label for="email">Email:</label>
+        <label htmlFor="email">Email:</label>
         <input id="email"></input>
 
-        <label for="phoneNumber">Phone Number:</label>
+        <label htmlFor="phoneNumber">Phone Number:</label>
         <input id="phoneNumber"></input>
 
-        <label for="address">Address:</label>
+        <label htmlFor="address">Address:</label>
         <input id="address"></input>
+      </form>
+
+      <form className="form" id="education">
+        <h1>Education</h1>
+
+        {!cardOpen && (
+          <p className="arrow" id="arrowdown" onClick={openCard}>
+            &darr;
+          </p>
+        )}
+
+        {cardOpen && (
+          <p className="arrow" id="arrowup" onClick={openCard}>
+            &uarr;
+          </p>
+        )}
+
+        {cardOpen && (
+          <div className="forms">
+            <label htmlFor="school">School:</label>
+            <input id="school"></input>
+
+            <label htmlFor="degree">Degree:</label>
+            <input id="degree"></input>
+
+            <div className="dates">
+              <div className="date">
+                <label htmlFor="startDate">Start Date:</label>
+                <input id="startDate" type="date"></input>
+              </div>
+
+              <div className="date">
+                <label htmlFor="endDate">End Date:</label>
+                <input id="endDate" type="date"></input>
+              </div>
+            </div>
+
+            <label htmlFor="location">Location:</label>
+            <input id="location"></input>
+          </div>
+        )}
       </form>
     </div>
   );
